239a637ab092d6d710819d04d12912b59638e4b9
UlugbekYarkinov <yarkinovulugbek@gmail.com>
2023-01-17 21:29:36 +0500

    bottom button added

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'components/reusable_card.dart';
 
+const Color activeCardColor = Color(0xFF1D1E33);
+const Color bottomCardColor = Color(0xFFEB1555);
+const double bottomHeight = 80.0;
+
 class InputPage extends StatefulWidget {
   @override
   State<InputPage> createState() => _InputPageState();
@@ -20,20 +24,26 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: Row(
               children: [
-                Expanded(child: ReusableCard(color: const Color(0xFF1D1E33),)),
-                Expanded(child: ReusableCard(color: const Color(0xFF1D1E33),))
+                Expanded(child: ReusableCard(color: activeCardColor)),
+                Expanded(child: ReusableCard(color: activeCardColor))
               ],
             ),
           ),
-          Expanded(child: ReusableCard(color: const Color(0xFF1D1E33),)),
+          Expanded(child: ReusableCard(color: activeCardColor)),
           Expanded(
             child: Row(
               children: [
-                Expanded(child: ReusableCard(color: const Color(0xFF1D1E33),)),
-                Expanded(child: ReusableCard(color: const Color(0xFF1D1E33),))
+                Expanded(child: ReusableCard(color: activeCardColor)),
+                Expanded(child: ReusableCard(color: activeCardColor))
               ],
             ),
           ),
+          Container(
+            margin: const EdgeInsets.only(top: 10.0),
+            color: bottomCardColor,
+            height: bottomHeight,
+            width: double.infinity,
+          )
         ],
       ),
     );
